4b279380681b43da53ccbc89ed333c21ae3b3256
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-11-24 00:02:05 +0100

    Implement spaceLeft()

--- src/app/mp3stego.js
+++ src/app/mp3stego.js
@@ -9,12 +9,27 @@ function MP3Stego(fileName, arrayBuffer) {
     var name = fileName;
     var mp3file = new MP3Parser(arrayBuffer);
 
+    var _countFrames = function() {
+        var frames = 0;
+        while (mp3file.hasNext()) {
+            frames++;
+            mp3file.nextFrame();
+        }
+        mp3file.seekStart();
+        return frames;
+    };
+
     this.isModified = function() {
 
     };
 
+    // Returns how many characters can be embedded
+    // 5 frames are reserved for signature (1 + 4)
+    // 13 frames are reserved for encryption (salt + IV)
     this.spaceLeft = function() {
-
+        var frameCount = _countFrames();
+        frameCount = frameCount - 5 - 13;
+        return Math.trunc(((frameCount * 5) + 1) / 8);
     };
 
     this.embed = function(message) {
