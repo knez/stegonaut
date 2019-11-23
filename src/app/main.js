ad6b892e1453fe7d9b94b9f97d42e3625478d166
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-11-23 01:38:26 +0100

    Add 'loadFile' event listener

---
+++ src/app/main.js
@@ -0,0 +1,17 @@
+var mp3;
+
+// Add event handlers
+document.getElementById("file")
+    .addEventListener("change", loadFile, true);
+
+// Load raw mp3 to buffer
+function loadFile() {
+    var reader = new FileReader();
+    var file = document.getElementById("file").files[0];
+    reader.onload = function() {
+        // mp3 = new MP3Stego(file.name, reader.result);
+        // document.getElementById("counter").value = mp3.spaceLeft();
+        alert(reader.result.byteLength);
+    };
+    reader.readAsArrayBuffer(file);
+}
