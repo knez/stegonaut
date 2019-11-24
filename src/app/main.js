13bbe2101de3f7cec104581dc11dfdc6c68d5c70
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-11-24 21:17:07 +0100

    Refactor accessing DOM elements

--- src/app/main.js
+++ src/app/main.js
@@ -1,20 +1,23 @@
 var mp3;
 
+// Init DOM elements
+var input = document.getElementById("input");
+var counter = document.getElementById("counter");
+
 // Add event handlers
-document.getElementById("file")
-    .addEventListener("change", loadFile, true);
+input.addEventListener("change", loadFile, true);
 
 // Load raw mp3 to buffer
 function loadFile() {
     var reader = new FileReader();
-    var file = document.getElementById("file").files[0];
+    var file = input.files[0];
     if (!file.name.endsWith(".mp3")) {
         alert("Not an MP3");
         return;
     }
     reader.onload = function() {
         mp3 = new MP3Stego(file.name, reader.result);
-        document.getElementById("counter").value = mp3.spaceLeft();
+        counter.value = mp3.spaceLeft();
     };
     reader.readAsArrayBuffer(file);
 }
