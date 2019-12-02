6605abe562ebcbc644be1d01699e21218ac7ab47
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-12-02 16:06:54 +0100

    Implement 'extractText' function

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,11 @@
-var mp3file, maxChars;
+var mp3file, mp3file2, maxChars;
 
 // Init DOM elements
 var input = document.getElementById("input");
+var input2 = document.getElementById("input2");
 var counter = document.getElementById("counter");
 var message = document.getElementById("message");
+var message2 = document.getElementById("message2");
 var embedButton = document.getElementById("embedButton");
 
 // Load raw mp3 to buffer
@@ -22,10 +24,32 @@ function loadFile() {
     reader.readAsArrayBuffer(file);
 }
 
+// Load raw mp3 to buffer
+function loadFile2() {
+    var reader = new FileReader();
+    var file = input2.files[0];
+    if (!file.name.endsWith(".mp3")) {
+        alert("Not an MP3");
+        return;
+    }
+    reader.onload = function() {
+        mp3file2 = new MP3Stego(file.name, reader.result);
+    };
+    reader.readAsArrayBuffer(file);
+}
+
 // Embed text into mp3 and trigger download
 function embedText() {
     message.disabled = embedButton.disabled = true;
     mp3file.embedText(message.value);
     mp3file.download();
     message.disabled = embedButton.disabled = false;
+}
+
+function extractText() {
+    if (mp3file2.isModified()) {
+        message2.value = mp3file2.extractText();
+    } else {
+        alert("Nothing to extract");
+    }
 }
