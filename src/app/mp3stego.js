3c175e7dbdeaec7b7488955a77071e077bcac94d
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-12-02 16:08:41 +0100

    Implement 'extractText' method

--- src/app/mp3stego.js
+++ src/app/mp3stego.js
@@ -22,12 +22,29 @@ function MP3Stego(fileName, arrayBuffer) {
         }
     };
 
+    // Get the number of frames to be read
+    var _getSignature = function() {
+        mp3.nextFrame();   // Skip flag
+        var num = 0;
+        for (var i = 0; i < 4; i++) {
+            num = num | _extract() << i * 5;
+            mp3.nextFrame();
+        }
+        return num;
+    };
+
     // Embed payload (5 bits) into frame header
     var _embed = function(payload) {
         var header = mp3.getFrameHeader();
         header[0] = (header[0] & 0xFE) | payload >> 4;
         header[1] = (header[1] & 0xF0) | payload & 0x0F;
         mp3.setFrameHeader(header);
+    };
+
+    // Extract payload from frame header
+    var _extract = function() {
+        var header = mp3.getFrameHeader();
+        return (header[0] & 0x01) << 4 | (header[1] & 0x0F);
     };
 
     var _countFrames = function() {
@@ -39,8 +56,10 @@ function MP3Stego(fileName, arrayBuffer) {
         return frames;
     };
 
+    // Check if file has been previously modified
     this.isModified = function() {
-
+        mp3.seekStart();
+        return _extract() == 0x1F;
     };
 
     // Returns how many characters can be embedded
@@ -63,8 +82,15 @@ function MP3Stego(fileName, arrayBuffer) {
         }
     };
 
+    // Extract message from frame headers
     this.extractText = function() {
-
+        var enc = [];
+        var frames = _getSignature();
+        for (var i = 0; i < frames; i++) {
+            enc.push(_extract());
+            mp3.nextFrame();
+        }
+        return Base32.decode(enc);
     };
 
     // Trigger the download of the newly created mp3 file
