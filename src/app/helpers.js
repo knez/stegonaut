3ac1e6a5cec2d64312de609a32e5be87ffd2e40e
Nikola Knežević <48497634+knez@users.noreply.github.com>
2019-12-16 14:13:10 +0100

    Fix linting problems

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -80,25 +80,26 @@ function encodeUTF8(str) {
 // Convert UTF-8 byte array -> UTF-16 string
 function decodeUTF8(bytes) {
     var out = [], pos = 0, c = 0;
+    var c1, c2, c3, c4;
     while (pos < bytes.length) {
-        var c1 = bytes[pos++];
+        c1 = bytes[pos++];
         if (c1 < 128) {
             out[c++] = String.fromCharCode(c1);
         } else if (c1 > 191 && c1 < 224) {
-            var c2 = bytes[pos++];
+            c2 = bytes[pos++];
             out[c++] = String.fromCharCode((c1 & 31) << 6 | c2 & 63);
         } else if (c1 > 239 && c1 < 365) {
             // Surrogate Pair
-            var c2 = bytes[pos++];
-            var c3 = bytes[pos++];
-            var c4 = bytes[pos++];
+            c2 = bytes[pos++];
+            c3 = bytes[pos++];
+            c4 = bytes[pos++];
             var u = ((c1 & 7) << 18 | (c2 & 63) << 12 | (c3 & 63) << 6 | c4 & 63) -
                 0x10000;
             out[c++] = String.fromCharCode(0xD800 + (u >> 10));
             out[c++] = String.fromCharCode(0xDC00 + (u & 1023));
         } else {
-            var c2 = bytes[pos++];
-            var c3 = bytes[pos++];
+            c2 = bytes[pos++];
+            c3 = bytes[pos++];
             out[c++] =
                 String.fromCharCode((c1 & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
         }
